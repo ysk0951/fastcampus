58ec3b67c3bfd045d9147327692efed31a44faa3
김영성 <yskimweb@gmail.com>
2021-10-25 17:50:41 +0900

    CH03_03

--- Javascript&Typescript Essential/HackerNews/app.js
+++ Javascript&Typescript Essential/HackerNews/app.js
@@ -1,16 +1,33 @@
 //let ajax = new XMLHttpRequest();   //변수
 const ajax = new XMLHttpRequest(); //상수
-const news_URL = 'https://api.hnpwa.com/v0/news/1.json'
-ajax.open('GET',news_URL,false);
+const container = document.getElementById('root');
+const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json'
+const CONTECT_URL = `https://api.hnpwa.com/v0/item/@id.json`;
+const content = document.createElement('div');
+ajax.open('GET',NEWS_URL,false);
 ajax.send();
 
 const newsFeeds = JSON.parse(ajax.response);
 const ul = document.createElement('ul');
+window.addEventListener('hashchange',function(){
+    let id = location.hash.substr(1);
+    ajax.open(`GET`,CONTECT_URL.replace('@id',id),false);
+    ajax.send();
+    const newsContent = JSON.parse(ajax.response);
+    const title = document.createElement('h1');
+    title.innerHTML = newsContent.title;
+    content.appendChild(title);
+})
 for(let i=0;i<newsFeeds.length;i++){
     const li = document.createElement('li');
-    li.innerHTML = newsFeeds[i].title;
+    const a = document.createElement('a');
+    a.href = `#${newsFeeds[i].id}`;
+    a.innerHTML = `${newsFeeds[i].title} (${newsFeeds[i].comments_count})`;
+    li.appendChild(a)
     ul.appendChild(li);
 }
-document.getElementById('root').appendChild(ul)
+container.appendChild(ul)
+container.appendChild(content)
+
 
 
