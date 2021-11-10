3e244766111fa006f248885f2eec13b8e2ba4e6e
yskim-web <abcm556@naver.com>
2021-11-10 23:04:07 +0900

    CH03_05

--- Javascript&Typescript Essential/HackerNews/app.js
+++ Javascript&Typescript Essential/HackerNews/app.js
@@ -5,8 +5,8 @@ const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json'
 const CONTECT_URL = `https://api.hnpwa.com/v0/item/@id.json`;
 const content = document.createElement('div');
 
-function getData(url){
-    ajax.open('GET',rul,false);
+function getData(url) {
+    ajax.open('GET', url, false);
     ajax.send();
     return JSON.parse(ajax.response);
 }
@@ -14,28 +14,42 @@ function getData(url){
 
 const newsFeeds = getData(NEWS_URL)
 const ul = document.createElement('ul');
-window.addEventListener('hashchange',function(){
+function newsDetail() {
     const id = location.hash.substr(1);
-    const newsContent = getData(CONTECT_URL.replace('@id',id))
+    const newsContent = getData(CONTECT_URL.replace('@id', id))
     const title = document.createElement('h1');
-    title.innerHTML = newsContent.title;
-    content.appendChild(title);
-})
-for(let i=0;i<newsFeeds.length;i++){
-    const div = document.createElement('div');
-    const li = document.createElement('li');
-    const a = document.createElement('a');
-    div.innerHTML = `
+
+    container.innerHTML = `
+        <h1>${newsContent.title}</h1>
+        <div>
+            <a href="#">목록으로</a>
+        </div>
+    `;
+}
+function newsFeed() {
+    const newsFeed = getData(NEWS_URL);
+    const newsList = [];
+    newsList.push('<ul>');
+    for (let i = 0; i < newsFeeds.length; i++) {
+        newsList.push(`
     <li>
         <a href="#${newsFeeds[i].id}">
             ${newsFeeds[i].title} (${newsFeeds[i].comments_count})
         </a>
     </li>
-    `;
-    ul.appendChild(div.firstElementChild)
+    `);
+    }
+    newsList.push('</ul>');
+    container.innerHTML = newsList.join('');
 }
-container.appendChild(ul)
-container.appendChild(content)
+function router(){
+    const routePath = location.hash;
+    if(routePath === ''){
+        newsFeed();
+    }else{
+        newsDetail();
+    }
+}
+window.addEventListener('hashchange', router);
 
-
-
+router();
