e8bb652c7d2e04cb2f7fe43b0a72a0483da7ef92
yskim-we <abcm556@naver.com>
2021-11-12 22:15:46 +0900

    CH04

---
+++ Javascript&Typescript Essential/Kit_Hacker_News_ts/src/core/api.ts
@@ -0,0 +1,35 @@
+import { NEWS_URL, CONTENT_URL } from '../config';
+import { NewsFeed, NewsDetail } from '../types';
+
+export default class Api {
+  url: string;
+
+  constructor(url: string) {
+    this.url = url;
+  }
+
+  async request<AjaxResponse>(): Promise<AjaxResponse> {
+    const response = await fetch(this.url);
+    return await response.json() as AjaxResponse;
+  }
+}
+
+export class NewsFeedApi extends Api {
+  constructor() {
+    super(NEWS_URL);
+  }
+
+  async getData(): Promise<NewsFeed[]> {
+    return this.request<NewsFeed[]>();
+  }
+}
+
+export class NewsDetailApi extends Api {
+  constructor(id: string) {
+    super(CONTENT_URL.replace('@id', id));
+  }
+
+  async getData(): Promise<NewsDetail> {
+    return this.request<NewsDetail>();
+  }
+}
